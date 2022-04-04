dc3706f45de874fcb7426b6ecc1656de21797b83
fukutada <92067046+fukutada@users.noreply.github.com>
2022-04-04 09:38:42 +0900

    up

---
+++ script.js
@@ -0,0 +1,33 @@
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+  });
+  
+  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+      $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+      $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+  });
+
+  // スクロールトップボタン
+scrollTop('js-scroll-top', 150); // 遅すぎるとガクガクになるので注意
+
+function scrollTop(el, duration) {
+  const target = document.getElementById(el);
+  target.addEventListener('click', function() {
+    let currentY = window.pageYOffset; // 現在のスクロール位置を取得
+    let step = duration/currentY > 1 ? 10 : 100; // 三項演算子
+    let timeStep = duration/currentY * step; // スクロール時間
+    let intervalId = setInterval(scrollUp, timeStep);
+    // timeStepの間隔でscrollUpを繰り返す。
+    // clearItervalのために返り値intervalIdを定義する。
+
+    function scrollUp(){
+      currentY = window.pageYOffset;
+      if(currentY === 0) {
+          clearInterval(intervalId); // ページ最上部に来たら終了
+      } else {
+          scrollBy( 0, -step ); // step分上へスクロール
+      }
+    }
+  });
+}
